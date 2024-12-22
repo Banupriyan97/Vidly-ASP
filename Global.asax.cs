81be8219c979538ce20cac929d8e2f762aff705a
Banupriyan <banuraveendran@gmail.com>
2024-12-22 09:40:43 +0530

    Build API for customers and movies

--- Global.asax.cs
+++ Global.asax.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
+using AutoMapper;
+using Vidly_ASP.App_Start;
 
 namespace Vidly_ASP
 {
@@ -12,6 +15,8 @@ namespace Vidly_ASP
     {
         protected void Application_Start()
         {
+            Mapper.Initialize(m => m.AddProfile<MappingProfile>());
+            GlobalConfiguration.Configure(WebApiConfig.Register); 
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
