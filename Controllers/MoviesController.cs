e47aab13bf6cc71bd143668df1daca4520451c54
Banupriyan <banuraveendran@gmail.com>
2024-12-23 15:04:53 +0530

    Add DataTables to movie list

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -97,9 +97,7 @@ namespace Vidly_ASP.Controllers
 
         public ActionResult Index()
         {
-            var movies = _context.Movies.Include(m => m.Genre).ToList();
-
-            return View(movies);
+            return View();
         }
 
         public ActionResult Details(int id)
